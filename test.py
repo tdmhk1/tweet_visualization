dde631e8947bdca55cf7b477cc173a68d6d59fa6
fjmn <tdmhk1@gmail.com>
2014-12-24 17:39:43 +0900

    twitter情報をDBへ格納

--- test.py
+++ test.py
@@ -9,6 +9,50 @@ import json
 import datetime
 import mysql.connector
 
+# UTC time からdatetime への変換
+def UTCtoDatetime(s):
+
+    YEAR = s[26:]
+    MOUTH = MouthtoInt(s[4:7])
+    DAY = s[8:10]
+    # 日本は世界標準時より９時間進んでいる
+    TIME = WorldtoJst(s[11:19])
+    t = YEAR+'-'+MOUTH+'-'+DAY+' '+TIME
+    return t
+
+# 月から数字を取得(出力は文字列)
+def MouthtoInt(s):
+    if s == 'Jan':
+        return ('01')
+    elif s == 'Feb':
+        return('02')
+    elif s == 'Mar':
+        return('03')
+    elif s == 'Apr':
+        return('04')
+    elif s == 'May':
+        return('05')
+    elif s == 'Jun':
+        return('06')
+    elif s == 'Jul':
+        return('07')
+    elif s == 'Aug':
+        return('08')
+    elif s == 'Sep':
+        return('09')
+    elif s == 'Oct':
+        return('10')
+    elif s == 'Nov':
+        return('11')
+    else:
+        return('12')
+
+# 世界標準時から日本時間に直す(９時間足す)
+def WorldtoJst(s):
+    h = (int(s[:2]) + 9) % 24
+    i = str("{0:02d}".format(h))
+    return s.replace(s[:2], i)
+
 # 開発目的でTwitter APIにアクセスする
 def oauth_login():
     CONSUMER_KEY = 'JciZvlRGhUtCnkf0Wj06TgWoS'
@@ -33,21 +77,6 @@ config = {
 connect = mysql.connector.connect(**config)
 cursor = connect.cursor()
 
-"""
-# サンプルデータの作成
-a = '3'
-b = '13'
-c = 'い'
-d = '2014-12-22 15:46:00'
-e = '103'
-"""
-
-cursor.execute('insert into tweets (tweet_id, user_id, text, create_at, retweeted_count) values(6, 4, "", "2014-12-23 10:04:00", 1)')
-cursor.close()
-connect.close()
-
-
-"""
 # twitter.Twitterのインスタンスが返される
 twitter_api = oauth_login()
 
@@ -57,30 +86,27 @@ twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
 # 日本のツイートを取得する
 stream = twitter_stream.statuses.filter(locations='129.482881, 31.285110, 147.852021, 44.908014')
 
+count = 0
 for tweet in stream:
-    a = tweet['id_str']
-    b = tweet['user']['id_str']
-    c = tweet['text']
-    d = tweet['created_at']
-    e = tweet['retweet_count']
 
-    #print(a+'\n'+b+'\n'+c+'\n'+d+'\n'+str(e)+'\n')
-    #now = datetime.datetime.now()
-    #print(now.strftime("%a %b %d %H:%M:%S %z %Y"))
-    d = datetime.datetime.strptime(d, "%a %b %d %H:%M:%S %z %Y")
-    print(str(d.year)+"-"+d.month+"-"+d.day+" "+d.hour+":"+d.minute+":"+d.second)
-    #d = d.strftime("%Y-%m-%d %H:%M:%S")
-    #d="2014-12-15 00:00:00"
-    # MySQLへの操作
-    cursor.execute('insert into tweets values('+a+',' +b+ ',"' +c+ '","'+d + '",' +e+')')
-    #cursor.execute('insert into tweets values(123456,123456,"aaあ","2014-12-15 00:00:00",123456)')
+    # 各カラムのデータを取得
+    TWEET_ID = str(tweet['id_str'])
+    USER_ID = str(tweet['user']['id_str'])
+    TEXT = tweet['text']
+    CREATED_AT = UTCtoDatetime(tweet['created_at'])
+    RETWEETED_COUNT = str(tweet['retweet_count'])
+
+    # SQL文の実行
+    cursor.execute('insert into tweets values('+TWEET_ID+', '+USER_ID+', "'+TEXT+'", "'+CREATED_AT+'", '+RETWEETED_COUNT+')')
+    # DBに反映
     connect.commit()
 
-    break
+    count += 1
+    if count > 100:
+        break
 
 cursor.close()
 connect.close()
-"""
 
 
 
