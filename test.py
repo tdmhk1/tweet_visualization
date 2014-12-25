665ff04de27dbb703ad923b33ca94aa73ad1f1bf
fjmn <tdmhk1@gmail.com>
2014-12-25 10:59:55 +0900

    UTCtimeからdatetimeへ直す部分を修正しました

--- test.py
+++ test.py
@@ -9,49 +9,15 @@ import json
 import datetime
 import mysql.connector
 
-# UTC time からdatetime への変換
-def UTCtoDatetime(s):
-
-    YEAR = s[26:]
-    MOUTH = MouthtoInt(s[4:7])
-    DAY = s[8:10]
-    # 日本は世界標準時より９時間進んでいる
-    TIME = WorldtoJst(s[11:19])
-    t = YEAR+'-'+MOUTH+'-'+DAY+' '+TIME
-    return t
-
 # 月から数字を取得(出力は文字列)
 def MouthtoInt(s):
-    if s == 'Jan':
-        return ('01')
-    elif s == 'Feb':
-        return('02')
-    elif s == 'Mar':
-        return('03')
-    elif s == 'Apr':
-        return('04')
-    elif s == 'May':
-        return('05')
-    elif s == 'Jun':
-        return('06')
-    elif s == 'Jul':
-        return('07')
-    elif s == 'Aug':
-        return('08')
-    elif s == 'Sep':
-        return('09')
-    elif s == 'Oct':
-        return('10')
-    elif s == 'Nov':
-        return('11')
-    else:
-        return('12')
+    dict = {"Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "Jun":6, "Jul":7, "Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12}
+    return dict[s]
 
-# 世界標準時から日本時間に直す(９時間足す)
-def WorldtoJst(s):
-    h = (int(s[:2]) + 9) % 24
-    i = str("{0:02d}".format(h))
-    return s.replace(s[:2], i)
+# UTC timeから、datetime型への変換(日本時間)
+def UTCtoDatetime(s):
+    t = datetime.datetime(int(s[26:]), MouthtoInt(s[4:7]), int(s[8:10]), int(s[11:13]), int(s[14:16]), int(s[17:19])) + datetime.timedelta(hours = 9)
+    return t
 
 # 開発目的でTwitter APIにアクセスする
 def oauth_login():
@@ -83,27 +49,22 @@ twitter_api = oauth_login()
 # self.authパラメータのリファレンス
 twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
 
-# 日本のツイートを取得する
-stream = twitter_stream.statuses.filter(locations='129.482881, 31.285110, 147.852021, 44.908014')
+# 名古屋市栄のツイートを取得する
+stream = twitter_stream.statuses.filter(locations='136.898159, 35.162542, 136.913866, 35.175171')
 
-count = 0
 for tweet in stream:
 
     # 各カラムのデータを取得
     TWEET_ID = str(tweet['id_str'])
     USER_ID = str(tweet['user']['id_str'])
     TEXT = tweet['text']
-    CREATED_AT = UTCtoDatetime(tweet['created_at'])
+    CREATED_AT = str(UTCtoDatetime(tweet['created_at']))
     RETWEETED_COUNT = str(tweet['retweet_count'])
 
     # SQL文の実行
     cursor.execute('insert into tweets values('+TWEET_ID+', '+USER_ID+', "'+TEXT+'", "'+CREATED_AT+'", '+RETWEETED_COUNT+')')
     # DBに反映
     connect.commit()
-
-    count += 1
-    if count > 100:
-        break
 
 cursor.close()
 connect.close()
