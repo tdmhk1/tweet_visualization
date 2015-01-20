eee99a2f8aeb94d89c8c4d569a00497d39123aeb
fjmn <tdmhk1@gmail.com>
2015-01-20 11:15:05 +0900

    10分ごとのツイート数の差分を計算するようにしました。

--- count_tweets.py
+++ count_tweets.py
@@ -1,15 +1,17 @@
-# -- coding: utf-8 --
+# 現在から10分前までさかのぼって、ツイート数を集計するプログラム
+#  -- coding: utf-8 --
 
 __author__ = 'Y.F'
 
 import datetime
 import mysql.connector
 
-connect = mysql.connector.connect(user='root', password='sm4547634', host='127.0.0.1', database='hotspot', port='3306')
-cursor = connect.cursor()
-
 # t_unit は、時間の刻み幅
 t_unit = datetime.timedelta(minutes=10)
+# 現在の時刻を取得
+t_1 = datetime.datetime.now()
+t_0 = t_1 - t_unit
+
 # 緯度、経度の刻み幅を設定(だいたい1km四方の正方形となるようにとった)
 lat_unit = 0.008983
 lng_unit = 0.011039
@@ -19,11 +21,11 @@ lng_inf = 139.545137
 lat_sup = 35.817059
 lng_sup = 139.921101
 
-# 現在の時刻を取得
-t_1 = datetime.datetime.now()
-t_0 = t_1 - t_unit
-
 lng_0 = lng_inf
+
+connect = mysql.connector.connect(user='root', password='sm4547634', host='127.0.0.1', database='hotspot', port='3306')
+cursor = connect.cursor()
+
 while True:
     lat_0 = lat_inf
     lng_1 = lng_0 + lng_unit
@@ -31,13 +33,20 @@ while True:
         lat_1 = lat_0 + lat_unit
         query1 = "select count(*) from tweets where created_at between '"+str(t_0)+"' and '"+str(t_1)+"' and lat between "+str(lat_0)+" and "+str(lat_1)+"and lng between "+str(lng_0)+" and "+str(lng_1)
         cursor.execute(query1)
+        # count は、現在から過去10分間に投稿されたツイート数
         count = cursor.fetchall()[0][0]
-        # point_lat, point_lng は、それぞれのブロックの代表点
-        point_lat = (lat_0 + lat_1) / 2
-        point_lng = (lng_0 + lng_1) / 2
-        # DBに集計結果を格納
-        query2 = 'insert into count (gettime, pointlat, pointlng, count) values ("'+str(t_1)[:19]+'", '+str(point_lat)+', '+str(point_lng)+', '+str(count)+')'
+        query2 = "select count from count where gettime = '"+str(t_0)+"' and pointlat = "+str(lat_0)+" and pointlng = "+str(lng_0)
         cursor.execute(query2)
+        try:
+            # countbは、20分前から10分前の間に投稿されたツイート数
+            countb = cursor.fetchall()[0][0]
+            # differenceは、現在と10分前のcountの差分
+            difference = count - countb
+        except IndexError:
+            difference = "null"
+        # DBに集計結果を格納
+        query3 = 'insert into count (gettime, pointlat, pointlng, count, difference) values ("'+str(t_1)[:19]+'", '+str(lat_0)+', '+str(lng_0)+', '+str(count)+', '+str(difference)+')'
+        cursor.execute(query3)
         connect.commit()
         lat_0 = lat_1
         if lat_1 > lat_sup:
