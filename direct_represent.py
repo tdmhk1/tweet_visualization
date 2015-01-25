4c07d036145051e43b9ce4f43ea33ac790af1776
fjmn <tdmhk1@gmail.com>
2015-01-25 16:29:32 +0900

    フロント部分の本番用です。

--- direct_represent.py
+++ direct_represent.py
@@ -28,16 +28,20 @@ def mapview():
         # サイトの訪問時刻の取得
         t_1 = datetime.datetime.now()
         t_0 = t_1 - t_unit
-        datatime=[str(t_0)[10:19], str(t_1)[10:19]]
+        t_00 = t_1 - datetime.timedelta(minutes=30)
         # 直近の更新時間(集計時間の確保のため、分の一の位が3を超えたとき更新する)
-        t_renew = floar_minute_ten(t_1 - datetime.timedelta(minutes=3))
+        t_renew_1 = floar_minute_ten(t_1 - datetime.timedelta(minutes=3))
+        t_renew_0 = t_renew_1 - datetime.timedelta(minutes=10)
+        datatime=[str(t_renew_0)[10:19], str(t_renew_1)[10:19]]
 
         import mysql.connector
         connect = mysql.connector.connect(user='root', password='sm4547634', host='127.0.0.1', database='hotspot', port='3306')
         cursor = connect.cursor()
+
         # 直近の更新時間のツイート数の取得
-        query1 = 'select pointlat, pointlng, counts from count_data where gettime = "'+str(t_renew)+'" order by counts desc'
+        query1 = 'select pointlat, pointlng, counts from count_data where gettime = "'+str(t_renew_1)+'" order by counts desc'
         cursor.execute(query1)
+
         # ツイート数の場所とツイート数が入った配列を作成
         count_data = []
         for column1 in cursor.fetchall():
@@ -46,8 +50,9 @@ def mapview():
         # ツイート数が最大の場所の緯度、経度を取得
         latmax = Decimal(count_data[0][0])
         lngmax = Decimal(count_data[0][1])
-        # ツイート数が最大の地域のツイートを取得
-        query2 = 'select * from tweets where created_at between "'+str(t_0)+'" and "'+str(t_1)+'" and lat between '+str(latmax)+'and '+str(latmax+lat_unit)+' and lng between '+str(lngmax)+'and '+str(lngmax+lng_unit)
+
+        # ツイート数が最大の地域のツイートを取得(過去30分間)
+        query2 = 'select * from tweets where created_at between "'+str(t_00)+'" and "'+str(t_1)+'" and lat between '+str(latmax)+'and '+str(latmax+lat_unit)+' and lng between '+str(lngmax)+'and '+str(lngmax+lng_unit)+'order by created_at desc'
         cursor.execute(query2)
         tweet_data_max = []
         for column2 in cursor.fetchall():
@@ -57,10 +62,63 @@ def mapview():
         query3 = 'select * from tweets where created_at between "'+str(t_0)+'" and "'+str(t_1)+'"'
         cursor.execute(query3)
         tweet_data = cursor.fetchall()
-        return render_template('direct_represent_web.html', tweet_data=tweet_data, tweet_data_max=tweet_data_max, datatime=datatime, count_data=count_data)
+
+        # リンクで表示する文章
+        linkdata = ['これから注目の場所のツイートに切り替え', "/trendspot"]
+        return render_template('direct_represent_web.html', tweet_data=tweet_data, tweet_data_max=tweet_data_max, datatime=datatime, count_data=count_data, linkdata=linkdata)
 
     except Exception as e:
                 return e
+
+@app.route('/trendspot')
+def trendspot():
+    try:
+
+        # サイトの訪問時刻の取得
+        t_1 = datetime.datetime.now()
+        t_0 = t_1 - t_unit
+        t_00 = t_1 - datetime.timedelta(minutes=30)
+        # 直近の更新時間(集計時間の確保のため、分の一の位が3を超えたとき更新する)
+        t_renew_1 = floar_minute_ten(t_1 - datetime.timedelta(minutes=3))
+        t_renew_0 = t_renew_1 - datetime.timedelta(minutes=10)
+        datatime=[str(t_renew_0)[10:19], str(t_renew_1)[10:19]]
+
+        import mysql.connector
+        connect = mysql.connector.connect(user='root', password='sm4547634', host='127.0.0.1', database='hotspot', port='3306')
+        cursor = connect.cursor()
+
+        # 直近の更新時間のツイート数の取得
+        query1 = 'select pointlat, pointlng, linear_expect from count_data where gettime = "'+str(t_renew_1)+'" order by linear_expect desc'
+        cursor.execute(query1)
+
+        # ツイート数の場所とツイート数が入った配列を作成
+        count_data = []
+        for column1 in cursor.fetchall():
+            count_data.append(column1)
+
+        # ツイート数が最大の場所の緯度、経度を取得
+        latmax = Decimal(count_data[0][0])
+        lngmax = Decimal(count_data[0][1])
+
+        # ツイート数が最大の地域のツイートを取得(過去30分間)
+        query2 = 'select * from tweets where created_at between "'+str(t_00)+'" and "'+str(t_1)+'" and lat between '+str(latmax)+'and '+str(latmax+lat_unit)+' and lng between '+str(lngmax)+'and '+str(lngmax+lng_unit)+'order by created_at desc'
+        cursor.execute(query2)
+        tweet_data_max = []
+        for column2 in cursor.fetchall():
+            tweet_data_max.append(column2)
+
+        # 直近10分間のツイートを取得
+        query3 = 'select * from tweets where created_at between "'+str(t_0)+'" and "'+str(t_1)+'"'
+        cursor.execute(query3)
+        tweet_data = cursor.fetchall()
+
+        # リンクで表示する文章
+        linkdata = ['今注目の場所のツイートへ切り替え', "../"]
+        return render_template('direct_represent_web.html', tweet_data=tweet_data, tweet_data_max=tweet_data_max, datatime=datatime, count_data=count_data, linkdata=linkdata)
+
+    except Exception as e:
+                return e
+
 
 if __name__=="__main__":
  app.run(debug=True)
